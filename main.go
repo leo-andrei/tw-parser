6e22f67e2f38bb986b346c69317bf5e699979315
Andrei Leonte <aleonte@fluidtrends.com>
2025-01-28 20:32:10 +0200

    shorten logs

--- main.go
+++ main.go
@@ -21,37 +21,16 @@ func main() {
 	signal.Notify(signalChan, os.Interrupt)
 
 	db := inmemory.New()
-	fmt.Println("============ Storage Info ============")
-	dbInfo, err := db.Info()
-	if err != nil {
-		fmt.Println("err", err)
-	}
-	fmt.Println("type", dbInfo.Name)
-	fmt.Println("===================================================")
 
 	ethClient := ethclient.New("https://ethereum-rpc.publicnode.com")
-	fmt.Println("============ ETH Client Info ============")
-	ethClientInfo, err := ethClient.Info()
-	if err != nil {
-		fmt.Println("err", err)
-	}
-	fmt.Println("apiVersion", ethClientInfo.ApiVersion)
-	fmt.Println("endpoint", ethClientInfo.Endpoint)
-	fmt.Println("===================================================")
 
 	startBlock := 19912329
-	fmt.Println("============ Start at Block ============")
-	fmt.Println("number", startBlock)
-	fmt.Println("===================================================")
 
 	parser := parser.New(ctx, db, ethClient, startBlock)
 
 	// get the current block number
-	fmt.Println("========== Current Block ============")
 	blockNumber := parser.GetCurrentBlock()
-	fmt.Println("number", blockNumber)
-	fmt.Println("===================================================")
-
+	fmt.Printf("Current Block : %d\n", blockNumber)
 	fmt.Println("Waiting 3 seconds for storage to be populated")
 	time.Sleep(3 * time.Second)
 
@@ -66,22 +45,21 @@ func main() {
 	}
 	key := keys[k.Int64()]
 
-	fmt.Printf("========== Get Transactions (address: %s) ============\n", key)
+	fmt.Printf("Get Transactions (address: %s) \n", key)
 	txs := parser.GetTransactions(key)
 	fmt.Printf("[\n")
 	for _, tx := range txs {
 		fmt.Printf("{\ntype: %s,\nhash: %s,\nfrom: %s,\nto: %s,\nblock: %s\n}\n", tx.Type, tx.Hash, tx.From, tx.To, tx.BlockNumber)
 	}
 	fmt.Printf("]")
-	fmt.Println("===================================================")
+	fmt.Printf("[\n")
 
-	fmt.Printf("========== Subscribe to Address (address: %s) ============\n", key)
+	fmt.Printf("Subscribe to Address (address: %s) \n", key)
 	if ok := parser.Subscribe(key); !ok {
 		fmt.Println("subscribe failed")
 	} else {
 		fmt.Println("Subscribed")
 	}
-	fmt.Println("===================================================")
 
 	// auto shutdown after 3 seconds
 	fmt.Println("Shutting down in 3 seconds...")
